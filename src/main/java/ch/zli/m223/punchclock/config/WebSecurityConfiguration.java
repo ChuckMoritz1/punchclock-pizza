2bb625296c6a8f1bf98555351bcab59d2848aa60
moritz <moritz.meier23.10@gamil.com>
2021-07-15 07:39:51 -0700

    login verknüpft

--- src/main/java/ch/zli/m223/punchclock/config/WebSecurityConfiguration.java
+++ src/main/java/ch/zli/m223/punchclock/config/WebSecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static ch.zli.m223.punchclock.config.SecurityConstants.LOG_IN_URL;
 import static ch.zli.m223.punchclock.config.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
@@ -32,6 +33,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html").permitAll()
+                .loginProcessingUrl(LOG_IN_URL)
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
